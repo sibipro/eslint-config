7164b819c082201c709033bc6437f7a6d4c25431
Joe Fleming <joe.fleming@gmail.com>
2019-09-25 16:56:37 -0700

    feat: add jsx-a11y rules to react

--- rules/react.js
+++ rules/react.js
@@ -1,6 +1,6 @@
 module.exports = {
-  plugins: ['react', 'react-hooks'],
-  extends: ['plugins:react/recommended'],
+  plugins: ['react', 'react-hooks', 'jsx-a11y'],
+  extends: ['plugins:react/recommended', 'plugin:jsx-a11y/recommended'],
   rules: {
     // react
     'react/destructuring-assignment': 'off',
