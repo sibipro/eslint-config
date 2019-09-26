acceadab639b2b01f3039d77a623bc8baf597d86
Joe Fleming <joe.fleming@gmail.com>
2019-09-25 17:51:08 -0700

    fix: overrides should be an array

--- rules/jest.js
+++ rules/jest.js
@@ -1,24 +1,26 @@
 module.exports = {
-  overrides: {
-    files: ['**/*.spec.{js,jsx,ts,tsx}'],
-    extends: ['plugin:jest/recommended'],
-    plugins: ['jest'],
-    env: {
-      'jest/globals': true,
+  overrides: [
+    {
+      files: ['**/*.spec.{js,jsx,ts,tsx}'],
+      extends: ['plugin:jest/recommended'],
+      plugins: ['jest'],
+      env: {
+        'jest/globals': true,
+      },
+      rules: {
+        'jest/expect-expect': 'error',
+        'jest/no-empty-title': 'error',
+        'jest/no-export': 'error',
+        'jest/no-if': 'error',
+        'jest/no-standalone-expect': 'error',
+        'jest/no-truthy-falsy': 'warn',
+        'jest/prefer-called-with': 'warn',
+        'jest/prefer-to-be-null': 'error',
+        'jest/prefer-to-be-undefined': 'error',
+        'jest/prefer-to-contain': 'error',
+        'jest/prefer-to-have-length': 'error',
+        'jest/require-top-level-describe': 'warn',
+      },
     },
-    rules: {
-      'jest/expect-expect': 'error',
-      'jest/no-empty-title': 'error',
-      'jest/no-export': 'error',
-      'jest/no-if': 'error',
-      'jest/no-standalone-expect': 'error',
-      'jest/no-truthy-falsy': 'warn',
-      'jest/prefer-called-with': 'warn',
-      'jest/prefer-to-be-null': 'error',
-      'jest/prefer-to-be-undefined': 'error',
-      'jest/prefer-to-contain': 'error',
-      'jest/prefer-to-have-length': 'error',
-      'jest/require-top-level-describe': 'warn',
-    },
-  },
+  ],
 };
--- rules/typescript.js
+++ rules/typescript.js
@@ -1,20 +1,22 @@
 module.exports = {
-  overrides: {
-    files: ['**/*.{ts,tsx}'],
-    plugins: ['import', '@typescript-eslint'],
-    extends: ['plugin:import/typescript', 'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
+  overrides: [
+    {
+      files: ['**/*.{ts,tsx}'],
+      plugins: ['import', '@typescript-eslint'],
+      extends: ['plugin:import/typescript', 'plugin:@typescript-eslint/recommended'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: './tsconfig.json',
+        includes: ['*.(j|t)sx?'],
       },
-      project: './tsconfig.json',
-      includes: ['*.(j|t)sx?'],
+      rules: {
+        'no-unused-vars': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      },
     },
-    rules: {
-      'no-unused-vars': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    },
-  },
+  ],
 };
