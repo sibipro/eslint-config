d5f894cbce9779ee577c98789607351b72b2e647
Joe Fleming <joe.fleming@gmail.com>
2019-10-01 17:13:29 -0700

    feat: add rules about missing and unused react props

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -3,13 +3,16 @@ module.exports = {
   extends: ['plugin:react/recommended', 'plugin:jsx-a11y/recommended'],
   rules: {
     // react
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
     'react/destructuring-assignment': 'off',
     'react/display-name': 'error',
     'react/forbid-prop-types': 'error',
     'react/jsx-closing-tag-location': 'off', // defer to prettier
     'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
-    'react/jsx-indent': 'off', // defer to prettier
     'react/jsx-indent-props': 'off', // defer to prettier
+    'react/jsx-indent': 'off', // defer to prettier
+    'react/jsx-no-duplicate-props': ['error', { ignoreCase: true }],
     'react/jsx-no-target-blank': 'error',
     'react/jsx-one-expression-per-line': 'off', // defer to prettier
     'react/jsx-uses-react': 'error',
@@ -17,8 +20,14 @@ module.exports = {
     'react/jsx-wrap-multilines': 'off', // defer to prettier
     'react/no-unescaped-entities': ['error', { forbid: ['>', '}'] }],
     'react/no-unsafe': 'warn',
+    'react/no-unused-prop-types': [
+      'error',
+      {
+        customValidators: [],
+        skipShapeProps: true,
+      },
+    ],
+    'react/prefer-stateless-function': ['error', { ignorePureComponents: true }],
     'react/static-property-placement': ['error', 'static public field'],
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
   },
 };
