e8cff24f47156cda41f9a101b1078221d4fee928
Joe Fleming <joe.fleming@gmail.com>
2019-10-08 17:56:17 -0700

    feat: tweak @typescript-eslint/no-unused-vars
    and allow ts-ignore in test files

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -1,21 +1,22 @@
 module.exports = {
   overrides: [
     {
-      files: ['**/*.{ts,tsx}'],
+      files: ['*.{ts,tsx}'],
       plugins: ['import', '@typescript-eslint'],
       extends: ['plugin:import/typescript', 'plugin:@typescript-eslint/recommended'],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-        project: './tsconfig.json',
-        includes: ['*.(j|t)sx?'],
-      },
       rules: {
         'no-unused-vars': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          { argsIgnorePattern: '^_', args: 'after-used', ignoreRestSiblings: true },
+        ],
+      },
+    },
+    {
+      files: ['*.spec.{ts,tsx}'],
+      rules: {
+        '@typescript-eslint/ban-ts-ignore': 'off',
       },
     },
   ],
