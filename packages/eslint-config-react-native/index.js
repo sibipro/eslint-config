990eb17efb237e6a8eb9df361f496b72663ab0f5
Andrew <me@iamandrew.io>
2019-10-17 12:33:00 -0700

    add some basic rules

--- packages/eslint-config-react-native/index.js
+++ packages/eslint-config-react-native/index.js
@@ -1,4 +1,7 @@
 module.exports = {
-  plugins: ['react-native'],
-  rules: {},
+  plugins: ['react-native', 'jsx-a11y'],
+  rules: {
+    'jsx-a11y/no-autofocus': 'off',
+    'react-native/no-unused-styles': 'error',
+  },
 };
