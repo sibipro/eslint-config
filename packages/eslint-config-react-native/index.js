a7afa06207d6433af473577ad1ed524e98ebd938
Andrew <me@iamandrew.io>
2019-10-17 17:03:28 -0700

    more react-native rules

--- packages/eslint-config-react-native/index.js
+++ packages/eslint-config-react-native/index.js
@@ -1,7 +1,15 @@
 module.exports = {
-  plugins: ['react-native', 'jsx-a11y'],
+  plugins: ['react-native', 'react', 'jsx-a11y'],
   rules: {
+    camelcase: 'off',
+    'global-require': 'off',
+    'no-extend-native': 'off',
+    'no-loop-func': 'off',
     'jsx-a11y/no-autofocus': 'off',
+    'react/forbid-prop-types': 'off',
     'react-native/no-unused-styles': 'error',
+    'no-param-reassign': 'off',
+    'no-async-promise-executor': 'off',
+    'func-names': 'off',
   },
 };
